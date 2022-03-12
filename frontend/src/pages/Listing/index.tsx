c4c730e4b50ebc7b0cd485833c176b96b20351ee
Rodrigo Martins <rodrigomartinsgb@hotmail.com>
2022-03-12 15:24:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,31 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
-function listing(){
-    return(
-        <Pagination />
+function listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div> 
+               </div>
+            </div>
+        </>
     );
 }
 
